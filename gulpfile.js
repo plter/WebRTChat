df83f2d877d6688c7492d43fe2243a69b5acab4a
plter <xtiqin@163.com>
2019-08-29 15:39:46 +0800

    添加Release目标

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,21 @@
 const gulp = require("gulp");
 
-module.exports.default = gulp.series(require("./SubProjects/FrontEnd/RTChat/gulpfile").default);
+global.BUILD_MODE = "development";
+
+function setBuildModeToProduction(cb) {
+    global.BUILD_MODE = "production";
+    cb();
+}
+
+function setBuildModeToDevelopment(cb) {
+    global.BUILD_MODE = "development";
+    cb();
+}
+
+const BuildRTChat = require("./SubProjects/FrontEnd/RTChat/gulpfile").default;
+
+module.exports = {
+    default: gulp.series(BuildRTChat),
+    BuildDebug: gulp.series(setBuildModeToDevelopment, BuildRTChat),
+    BuildRelease: gulp.series(setBuildModeToProduction, BuildRTChat)
+};
